e672d7e245871b1fed064b296b0306ace49b06f9
gopro1x <gopro1x@outlook.com>
2026-05-22 12:37:36 +0600

    Cleanup temporary node scripts

--- check_hww.js
+++
@@ -1,3 +0,0 @@
-import { execSync } from 'child_process';
-const oldHowWeWorkContent = execSync('git show 9e8d598^1:how-we-work.html').toString('utf8');
-console.log(oldHowWeWorkContent.includes('cat-home-kitchen.png') ? 'how-we-work had images' : 'how-we-work did NOT have images');
